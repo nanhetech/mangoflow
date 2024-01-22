b845750d1cbb7a83fb2fb8289159fa64ffdee148
Henry Zhang <hello@zhanghe.dev>
2024-01-22 12:23:28 +0800

    Remove unused code and update default locale

--- src/background/index.ts
+++ src/background/index.ts
@@ -12,22 +12,3 @@ chrome.action.onClicked.addListener(async (tab) => {
     openPanelOnActionClick: true,
   }).catch(console.error)
 })
-
-chrome.commands.onCommand.addListener(async (command, tab) => {
-  if (command === "open") {
-    if (!chrome.sidePanel) {
-      chrome.runtime.openOptionsPage();
-      return;
-    }
-    // console.info("tab", await chrome.sidePanel.getOptions({ tabId: tab.id }))
-    chrome.sidePanel.open({ windowId: tab.windowId });
-    chrome.sidePanel.setOptions({
-      tabId: tab.id,
-      path: 'sidepanel.html',
-      enabled: true
-    });
-    // chrome.sidePanel.setPanelBehavior({
-    //   openPanelOnActionClick: true,
-    // }).catch(console.error)
-  }
-})
